159f5f273ced4fbe90651ef50c5e19f3905e011e
Nerijus Urbonas <nerijus.urbonas@gmail.com>
2022-06-11 12:27:39 +0200

    add header component

--- src/components/Dropdown.jsx
+++ src/components/Dropdown.jsx
@@ -1,7 +1,24 @@
-import { useState } from "react";
+import { useState, useEffect, useRef } from "react";
 
-const Dropdown = ({ options, selected, onSelectedChange }) => {
+const Dropdown = ({ label, options, selected, onSelectedChange }) => {
   const [open, setOpen] = useState(false);
+  const ref = useRef();
+
+  useEffect(() => {
+    const onBodyClick = (event) => {
+      if (ref.current.contains(event.target)) {
+        return;
+      }
+      setOpen(false);
+    };
+    document.body.addEventListener("click", onBodyClick, { capture: true });
+ 
+    return () => {
+      document.body.removeEventListener("click", onBodyClick, {
+        capture: true,
+      });
+    };
+  }, []);
 
   const renderedOptions = options.map((option) => {
     if (option.value === selected.value) {
@@ -20,10 +37,10 @@ const Dropdown = ({ options, selected, onSelectedChange }) => {
   });
 
   return (
-    <div className="ui form">
+    <div ref={ref} className="ui form">
       <div className="field">
         <label htmlFor="" className="label">
-          Select a Color
+          {label}
         </label>
         <div
           onClick={() => setOpen(!open)}
