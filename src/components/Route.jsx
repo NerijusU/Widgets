3f00d597954afda259b5454d5b432743ae9d9c8c
Nerijus Urbonas <nerijus.urbonas@gmail.com>
2022-06-11 13:06:04 +0200

    update Link component

--- src/components/Route.jsx
+++ src/components/Route.jsx
@@ -1,9 +1,11 @@
 import { useEffect, useState } from "react";
 
 const Route = ({ path, children }) => {
+  const [currentPath, setCurrentPath] = useState(window.location.pathname);
+
   useEffect(() => {
     const onLocationChange = () => {
-      console.log('Location Change')
+      setCurrentPath(window.location.pathname)
     }
 
     window.addEventListener('popstate', onLocationChange);
@@ -13,7 +15,8 @@ const Route = ({ path, children }) => {
     }
   }, [])
 
-  return window.location.pathname === path ? children : null;
+  // return window.location.pathname === path ? children : null;
+  return currentPath === path ? children : null;
 };
 
 export default Route;
